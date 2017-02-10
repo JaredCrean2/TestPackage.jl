851e434c2f8c3a077d4d443978b643d357a51c6e
jared <jcrean01@gmail.com>
2017-02-09 22:06:39 -0500

    add some code

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -1,5 +1,31 @@
+__precompile__
 module TestPackage
 
 # package code goes here
+
+global const val = 42
+
+function myfunc0()
+  println("a = ", 42)
+end
+
+function myfunc(a::Int)
+  println("a = ", a)
+end
+
+function myfunc2(a::Int, b::Int)
+  return a + b
+end
+
+function myfunc3(a, b)
+  return a + b
+end
+
+function myfunc4{Tsol}(a::AbstractArray, b::AbstractArray{Tsol})
+  println("a = \n", a)
+  println("b = \n", b)
+
+  return nothing
+end
 
 end # module
